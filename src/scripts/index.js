0396a8ffcfb25fcf7ad2d8ecaf88ad8e39012355
Nery <felipe.nery@facamara.com.br>
2019-08-15 15:00:59 -0300

    finalizado logica de calculo e ajuste de horas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,49 @@
 let horas, minutos = "";
+let diaSeguinte = false;
+
+let horaResultado = 0;
+let minutoResultado = 0;
 
 function atualizaHorario() {
-    horas = document.getElementById('txtHora').value;
-    minutos = document.getElementById('txtMinuto').value;
+    horas = parseInt(document.getElementById('txtHora').value);
+    minutos = parseInt(document.getElementById('txtMinuto').value);
+
+    formataHoraMinutos();
+
+    if(diaSeguinte){
+        document.getElementById('horario-saida').innerText = `${horaResultado}:${minutoResultado} do dia seguinte`;
+    } else {
+        document.getElementById('horario-saida').innerText = `${horaResultado}:${minutoResultado}`;
+    }
+}
+
+function formataHoraMinutos(){
+    if(horas <= 24 && minutos <= 59){
+        horaResultado = horas + 9;
+        minutoResultado = minutos;
+    
+        if (horaResultado > 23) {
+            horaResultado = horaResultado - 24;
+            diaSeguinte = true;
+        }
+        else{
+            diaSeguinte = false;
+        }
+    
+        if(horaResultado.toString().length == 1) {
+            horaResultado = `0${horaResultado}`;
+        }
+    
+        if(minutoResultado.toString().length == 1){
+            minutoResultado = `0${minutoResultado}`;
+        }
+    }
+    else{
+        horaResultado = "00";
+        minutoResultado = "00";
+
+        document.getElementById('txtHora').value = '00'
+        document.getElementById('txtMinuto').value = '00'
+    }
+    
 }
